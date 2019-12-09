9a9fe6091748e673b52e5a7101b2bb7cfe2bb73a
Sascha Corti <sascha@corti.com>
2019-12-09 17:24:24 +0100

    Add compatibility to .NET Core 3.1

--- IotHubSync.Service/Startup.cs
+++ IotHubSync.Service/Startup.cs
@@ -24,7 +24,7 @@ namespace IotHubSync.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddHostedService<SyncBackgroundService>();
 
             services.AddSingleton<IDeviceSynchronizerSingleton, DeviceSynchronizerSingleton>();
